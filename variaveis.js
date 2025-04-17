e523a109d90cd52655a3de0faf2156cd90dcd7e5
ENZO GABRIEL ARAÚJO DOS SANTOS <enzo_ga_santos@estudante.sesisenai.org.br>
2025-04-16 22:18:53 -0300

    declaração de variáveis

---
+++ variaveis.js
@@ -0,0 +1,20 @@
+//declaração de variável com var
+
+// var nome = "Enzo"
+
+/* var nome = "João"*/
+
+//declaração de variável co let
+
+// let nome = "Enzo"
+
+// nome = "João"
+
+// variável declarada com const
+
+const nome = "Enzin"
+
+nome = "John Magnus Alexader"
+
+ console.log(nome) 
+
